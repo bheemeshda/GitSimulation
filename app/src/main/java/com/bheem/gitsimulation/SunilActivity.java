656363a950e7b697fabef0fcb21f07cf2add850f
sunilkumarsc <sunil.sc@paytm.com>
2016-02-11 17:35:51 +0530

    Sunil_added_newActivity

---
+++ app/src/main/java/com/bheem/gitsimulation/SunilActivity.java
@@ -0,0 +1,15 @@
+package com.bheem.gitsimulation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sunil);
+
+        System.out.println("Sunil ACtivity loaded");
+    }
+}
